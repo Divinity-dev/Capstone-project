febe1a9fed5da37830b1efeb59cdb4042e64680f
Divine Asiriuwa <divine_asiriuwa@yahoo.com>
2023-01-11 15:57:04 +0100

    Add html, css and js files

---
+++ app.js
@@ -0,0 +1,64 @@
+document.getElementById('hamburger').onclick = () => {
+  document.getElementById('pop-up').classList.add('show');
+  document.getElementById('hamburger').classList.add('close');
+};
+
+document.getElementById('close').onclick = () => {
+  document.getElementById('pop-up').classList.remove('show');
+  document.getElementById('hamburger').classList.remove('close');
+};
+
+document.getElementById('nav').onclick = () => {
+  document.getElementById('pop-up').classList.remove('show');
+  document.getElementById('hamburger').classList.remove('close');
+};
+
+document.getElementById('nav1').onclick = () => {
+  document.getElementById('pop-up').classList.remove('show');
+  document.getElementById('hamburger').classList.remove('close');
+};
+
+document.getElementById('nav2').onclick = () => {
+  document.getElementById('pop-up').classList.remove('show');
+  document.getElementById('hamburger').classList.remove('close');
+};
+
+document.getElementById('nav3').onclick = () => {
+  document.getElementById('pop-up').classList.remove('show');
+  document.getElementById('hamburger').classList.remove('close');
+};
+
+document.getElementById('nav4').onclick = () => {
+  document.getElementById('pop-up').classList.remove('show');
+  document.getElementById('hamburger').classList.remove('close');
+};
+
+document.getElementById('nav5').onclick = () => {
+  document.getElementById('pop-up').classList.remove('show');
+  document.getElementById('hamburger').classList.remove('close');
+};
+
+document.getElementById('name1').innerHTML = 'Adeyemi Folorunso';
+document.getElementById('name2').innerHTML = 'Rachael owaleye';
+document.getElementById('name3').innerHTML = 'Kulmar orvile';
+document.getElementById('name4').innerHTML = 'Ediagbonya kennedy';
+document.getElementById('name5').innerHTML = 'Okechukwu Eyinaya';
+document.getElementById('name6').innerHTML = 'Asisat Yakubu';
+document.getElementById('d1').innerHTML = 'Associate professor at the university of Ibadan';
+document.getElementById('d2').innerHTML = 'Specialist in animal husbadary';
+document.getElementById('d3').innerHTML = 'Specialistin plant biology and botany';
+document.getElementById('d4').innerHTML = 'C.E.O Ken foods Nigeria LMTD';
+document.getElementById('d5').innerHTML = 'Senior lecturer at Nigerian university(Nsuka)';
+document.getElementById('d6').innerHTML = 'A trained Zoologist at the university of benin';
+document.getElementById('t1').innerHTML = 'Vice-chairman, Nigeria Farmers Association ';
+document.getElementById('t2').innerHTML = 'Studied animal science at the university of benin.C.E.O Rach industries';
+document.getElementById('t3').innerHTML = 'professor of crop science, universityof ibadan. A season author of sevral books.';
+document.getElementById('t4').innerHTML = 'A top Agro-preneur in Nigeria';
+document.getElementById('t5').innerHTML = 'Chairman, Nigeria Framers Association';
+document.getElementById('t6').innerHTML = 'Multiple award winner, farmer of the year.';
+document.getElementById('img1').src = 'images/p5.webp';
+document.getElementById('img2').src = 'images/p1.jpg';
+document.getElementById('img3').src = 'images/p2.webp';
+document.getElementById('img4').src = 'images/p4.webp';
+document.getElementById('img5').src = 'images/p3.webp';
+document.getElementById('img6').src = 'images/p6.webp';
